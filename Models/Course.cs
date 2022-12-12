afad560d6155a6485ad2a77c63415b6562924da1
Fernando D'luccas <dluccash@hotmail.com>
2022-12-12 11:51:57 -0300

    feat: add Course model

---
+++ Models/Course.cs
@@ -0,0 +1,12 @@
+namespace CRMEducacional.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public List<Enrollment>? Enrollments { get; set; }
+    }
+}
